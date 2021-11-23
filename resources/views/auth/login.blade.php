10074f3aa2d01018432a0edd710efb5012f6bcba
HammamZarefa <HammamZarefa@gmail.com>
2021-11-23 15:10:22 +0200

    fix admin img

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Vanjava - Admin Login</title>
+  <title>Damasco - Admin Login</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
